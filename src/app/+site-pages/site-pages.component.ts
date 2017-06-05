55bd8ea27edb801d015cbb335a7964755c5f86f1
Nikolay Popov <mail@lifebits.ru>
2017-06-05 22:07:13 +0700

    refactoring

--- src/app/+site-pages/site-pages.component.ts
+++ src/app/+site-pages/site-pages.component.ts
@@ -1,26 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import {DomSanitizer} from '@angular/platform-browser';
-import {MdIconRegistry} from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MdIconRegistry } from '@angular/material';
 
 @Component({
-  selector: 'app-site-pages',
-  templateUrl: './site-pages.component.html',
-  styleUrls: ['./site-pages.component.scss']
+   selector: 'app-site-pages',
+   templateUrl: './site-pages.component.html',
+   styleUrls: ['./site-pages.component.scss']
 })
 export class SitePagesComponent implements OnInit {
 
-  constructor(
-     private iconRegistry: MdIconRegistry,
-     private sanitizer: DomSanitizer) {
+   constructor(
+      private iconRegistry: MdIconRegistry,
+      private sanitizer: DomSanitizer) {
 
-     iconRegistry
-        .addSvgIcon('aldenta-logo',
-           sanitizer.bypassSecurityTrustResourceUrl('assets/svg/aldenta-logo.svg'))
-        .addSvgIconSetInNamespace('home-page',
-           sanitizer.bypassSecurityTrustResourceUrl('assets/svg/sets/home-page-icon.svg'));
-  }
+      iconRegistry
+         .addSvgIcon('aldenta-logo',
+            sanitizer.bypassSecurityTrustResourceUrl('assets/svg/aldenta-logo.svg'))
+         .addSvgIconSetInNamespace('home-page',
+            sanitizer.bypassSecurityTrustResourceUrl('assets/svg/sets/home-page-icon.svg'));
+   }
 
-  ngOnInit() {
-  }
+   ngOnInit() {
+   }
 
 }
