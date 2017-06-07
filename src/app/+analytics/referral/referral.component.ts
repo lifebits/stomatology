d293eef3be7e7c236ed231a672542a8f42daf789
Nikolay Popov <mail@lifebits.ru>
2017-06-07 13:34:36 +0700

    added sorting table fields

--- src/app/+analytics/referral/referral.component.ts
+++ src/app/+analytics/referral/referral.component.ts
@@ -8,7 +8,46 @@ import { ReferralService } from './referral.service';
 })
 export class ReferralComponent implements OnInit {
 
-   data;
+   referralList;
+   tableFields = [
+      {
+         name: 'Дата обращения',
+         title: 'Дата обращения',
+         svg: 'analytics:sort-ascending',
+         active: false,
+         ascSort: true
+      }, {
+         name: 'Администратор',
+         title: 'Администратор',
+         svg: 'analytics:sort',
+         active: false,
+         ascSort: true
+      }, {
+         name: 'Источник обращения',
+         title: 'Источник обращения',
+         svg: 'analytics:sort',
+         active: false,
+         ascSort: true
+      }, {
+         name: 'Фамилия',
+         title: 'Пациент',
+         svg: 'analytics:sort',
+         active: false,
+         ascSort: true
+      }, {
+         name: 'Записан на первичную консультацию',
+         title: 'Запись на консультацию',
+         svg: 'analytics:sort',
+         active: false,
+         ascSort: true
+      }, {
+         name: 'Фамилия врача',
+         title: 'Фамилия врача',
+         svg: 'analytics:sort',
+         active: false,
+         ascSort: true
+      }
+   ];
 
    constructor(
       private referral: ReferralService) {
@@ -18,10 +57,70 @@ export class ReferralComponent implements OnInit {
    ngOnInit() {
       this.referral.getDirectedPatients().subscribe(
          result => {
-            this.data = result;
-            console.log(result);
+            this.referralList = result.sort(this.ascSort('Дата обращения'));
          }
       );
    }
 
+   setActiveSortField(tableFieldIndex) {
+      const tableField = this.tableFields[tableFieldIndex];
+
+      if (!tableField.active) {
+         this.tableFields.map(item => resetActiveStatus(item));
+         tableField.active = true;
+      } else {
+         tableField.ascSort = !tableField.ascSort;
+      }
+
+      tableField.svg = getSVG(tableField.active, tableField.ascSort);
+
+      this.tableFields.splice(tableFieldIndex, 1, tableField);
+
+      (tableField.ascSort) ? this.referralList.sort(this.ascSort(tableField.name)) : this.referralList.sort(this.descSort(tableField.name));
+
+
+      function resetActiveStatus(item) {
+         item.active = false;
+         item.svg = getSVG(item.active, item.ascSort);
+         return item;
+      }
+
+      function getSVG(activeStatus, ascSortStatus): string {
+         if (!activeStatus) {
+            return 'analytics:sort';
+         }
+         if (activeStatus && ascSortStatus) {
+            return 'analytics:sort-ascending';
+         }
+         if (activeStatus && !ascSortStatus) {
+            return 'analytics:sort-descending';
+         }
+      }
+   }
+
+   private ascSort(fieldName: string): Function {
+      console.log('run ascSort', fieldName);
+      return function(a, b) {
+         if (a[fieldName] > b[fieldName]) {
+            return 1;
+         }
+         if (a[fieldName] < b[fieldName]) {
+            return -1;
+         }
+         return 0;
+      };
+   }
+
+   private descSort(fieldName: string): Function {
+      console.log('run descSort', fieldName);
+      return function(a, b) {
+         if (a[fieldName] > b[fieldName]) {
+            return -1;
+         }
+         if (a[fieldName] < b[fieldName]) {
+            return 1;
+         }
+         return 0;
+      };
+   }
 }
