ab2cf82a88f98c65ff5385962eb3a2f400d70037
Nikolay Popov <mail@lifebits.ru>
2017-06-12 19:35:28 +0700

    refactoring analytics-header component

--- src/app/+analytics/components/analytics-header/analytics-header.component.ts
+++ src/app/+analytics/components/analytics-header/analytics-header.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-analytics-header',
-  templateUrl: './analytics-header.component.html',
-  styleUrls: ['./analytics-header.component.scss']
+   selector: 'app-analytics-header',
+   templateUrl: './analytics-header.component.html',
+   styleUrls: ['./analytics-header.component.scss']
 })
 export class AnalyticsHeaderComponent implements OnInit {
 
-  constructor() { }
+   constructor() {
 
-  ngOnInit() {
-  }
+   }
+
+   ngOnInit() {
+
+   }
 
 }
