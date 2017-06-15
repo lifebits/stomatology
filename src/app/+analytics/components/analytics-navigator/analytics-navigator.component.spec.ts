eca5656c2fa651645ffc8c242bf5147505e5d031
Nikolay Popov <mail@lifebits.ru>
2017-06-15 09:38:20 +0700

    updated analytics-navigator.component.spec

--- src/app/+analytics/components/analytics-navigator/analytics-navigator.component.spec.ts
+++ src/app/+analytics/components/analytics-navigator/analytics-navigator.component.spec.ts
@@ -1,25 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
-import { AnalyticsNavigatorComponent } from './analytics-navigator.component';
+import { MdIconModule } from '@angular/material';
+
+import {AnalyticsNavigatorComponent} from './analytics-navigator.component';
 
 describe('AnalyticsNavigatorComponent', () => {
-  let component: AnalyticsNavigatorComponent;
-  let fixture: ComponentFixture<AnalyticsNavigatorComponent>;
+   let component: AnalyticsNavigatorComponent;
+   let fixture: ComponentFixture<AnalyticsNavigatorComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AnalyticsNavigatorComponent ]
-    })
-    .compileComponents();
-  }));
+   beforeEach(async(() => {
+      TestBed.configureTestingModule({
+         imports: [ RouterTestingModule, MdIconModule ],
+         declarations: [ AnalyticsNavigatorComponent ]
+      })
+         .compileComponents();
+   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AnalyticsNavigatorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+   beforeEach(() => {
+      fixture = TestBed.createComponent(AnalyticsNavigatorComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+   it('should be created', () => {
+      expect(component).toBeTruthy();
+   });
 });
