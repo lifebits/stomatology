d1cb8d2c2453b1a852bafc3c392eeae69b46dc91
Nikolay Popov <mail@lifebits.ru>
2017-06-16 12:35:07 +0700

    updated spec orthodontics.component and orthopedics.component

--- src/app/+analytics/orthodontics/orthodontics.component.spec.ts
+++ src/app/+analytics/orthodontics/orthodontics.component.spec.ts
@@ -1,25 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DatePipe, DecimalPipe } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdCardModule, MdInputModule, MdIconModule } from '@angular/material';
 
 import { OrthodonticsComponent } from './orthodontics.component';
+import { DataTableComponent } from '../components/data-table/data-table.component';
+import { CellFormatPipe } from '../components/data-table/cell-format.pipe';
+import { SortingService } from '../../services/sorting/sorting.service';
 
 describe('OrthodonticsComponent', () => {
-  let component: OrthodonticsComponent;
-  let fixture: ComponentFixture<OrthodonticsComponent>;
+   let component: OrthodonticsComponent;
+   let fixture: ComponentFixture<OrthodonticsComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ OrthodonticsComponent ]
-    })
-    .compileComponents();
-  }));
+   beforeEach(async(() => {
+      TestBed.configureTestingModule({
+         imports: [ HttpModule, ReactiveFormsModule, BrowserAnimationsModule, MdCardModule, MdInputModule, MdIconModule ],
+         declarations: [ OrthodonticsComponent, DataTableComponent, CellFormatPipe ],
+         providers: [ SortingService, DatePipe, DecimalPipe ]
+      })
+         .compileComponents();
+   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(OrthodonticsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+   beforeEach(() => {
+      fixture = TestBed.createComponent(OrthodonticsComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+   it('should be created', () => {
+      expect(component).toBeTruthy();
+   });
 });
--- src/app/+analytics/orthopedics/orthopedics.component.spec.ts
+++ src/app/+analytics/orthopedics/orthopedics.component.spec.ts
@@ -1,25 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DatePipe, DecimalPipe } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdCardModule, MdInputModule, MdIconModule } from '@angular/material';
 
 import { OrthopedicsComponent } from './orthopedics.component';
+import { DataTableComponent } from '../components/data-table/data-table.component';
+import { CellFormatPipe } from '../components/data-table/cell-format.pipe';
+import { SortingService } from '../../services/sorting/sorting.service';
 
 describe('OrthopedicsComponent', () => {
-  let component: OrthopedicsComponent;
-  let fixture: ComponentFixture<OrthopedicsComponent>;
+   let component: OrthopedicsComponent;
+   let fixture: ComponentFixture<OrthopedicsComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ OrthopedicsComponent ]
-    })
-    .compileComponents();
-  }));
+   beforeEach(async(() => {
+      TestBed.configureTestingModule({
+         imports: [ HttpModule, ReactiveFormsModule, MdCardModule, MdInputModule, MdIconModule, BrowserAnimationsModule ],
+         declarations: [ OrthopedicsComponent, DataTableComponent, CellFormatPipe ],
+         providers: [ SortingService, DatePipe, DecimalPipe ]
+      })
+         .compileComponents();
+   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(OrthopedicsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+   beforeEach(() => {
+      fixture = TestBed.createComponent(OrthopedicsComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+   it('should be created', () => {
+      expect(component).toBeTruthy();
+   });
 });
