d6ec27a61e88c8d9ee427efc724573458fbfbceb
Nikolay Popov <mail@lifebits.ru>
2017-06-19 23:30:07 +0700

    added ChartOptions Interface

--- src/app/services/chart/charts.service.ts
+++ src/app/services/chart/charts.service.ts
@@ -1,12 +1,37 @@
 import { Injectable } from '@angular/core';
 import * as Highcharts from 'highcharts';
 
+export interface ChartsOptions {
+   chart?: {
+      type: string;
+   };
+   title: {
+      text: string;
+   };
+   yAxis: {
+      title: {
+         text: string;
+      }
+   };
+   xAxis: {
+      type?: string;
+      dateTimeLabelFormats?: {
+         month: string;
+         year: string;
+      },
+      title: {
+         text: string;
+      }
+   };
+   series: any[];
+}
+
 @Injectable()
 export class ChartsService {
 
   constructor() {}
 
-  draw(container: string, opts: Object) {
+  draw(container: string, opts: ChartsOptions) {
      Highcharts.chart('container', opts);
   }
 
