912e6f3b2fc9a2a4242a041a6667cfa28a26ea86
Nikolay Popov <mail@lifebits.ru>
2017-06-22 07:32:37 +0700

    updated date-range service

--- src/app/+analytics/components/date-range-selection/date-range.service.ts
+++ src/app/+analytics/components/date-range-selection/date-range.service.ts
@@ -1,10 +1,40 @@
 import { Injectable } from '@angular/core';
-import { DateRange } from '../../analytics.service';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+export interface DateRange {
+   startDate: Date;
+   endDate: Date;
+}
 
 @Injectable()
 export class DateRangeService {
 
+   private currentDateRangeSource = new BehaviorSubject(this.getTestedDateRange());
+   currentDateRange$ = this.currentDateRangeSource.asObservable();
+
    constructor() {}
+
+   setCurrentDateRange(value: DateRange) {
+      console.log('Set currentDateRangeSource: ', value);
+      this.currentDateRangeSource.next(value);
+   }
+
+   dataFilteringByDate(data: Object[], dataField: string, dateRange?: DateRange): Object[] {
+      const _dateRange = dateRange || this.currentDateRangeSource.getValue();
+      const startDate = +_dateRange.startDate;
+      const endDate = +_dateRange.endDate;
+      return data.filter(a => {
+         const dateReferral = +new Date(a[dataField]);
+         return (dateReferral >= startDate && dateReferral <= endDate);
+      });
+   }
+
+   getTestedDateRange(): DateRange {
+      return {
+         startDate: new Date(2017, 0, 17),
+         endDate: new Date(2017, 5, 18)
+      };
+   }
 
    getCurrentDayRange(): DateRange {
       const currentDay = new Date();
@@ -21,7 +51,6 @@ export class DateRangeService {
       const dayOfWeek = currentDay.getDay();
       const dayOfMonth = currentDay.getDate();
       const result = new Date().setDate(dayOfMonth - dayOfWeek + 1);
-
       return {
          startDate: new Date(result),
          endDate: new Date()
@@ -32,6 +61,26 @@ export class DateRangeService {
       const startDay = new Date().setDate(1);
       return {
          startDate: new Date(startDay),
+         endDate: new Date()
+      };
+   }
+
+   getCurrentQuarterRange() {
+      const currentDay = new Date();
+      const monthNumber = currentDay.getMonth();
+      const quarter = new Date().setMonth(monthNumber - 2, 1);
+      return {
+         startDate: new Date(quarter),
+         endDate: new Date()
+      };
+   }
+
+   getCurrentYearRange(): DateRange {
+      const currentDay = new Date();
+      const currentYear = currentDay.getFullYear();
+      const year = new Date().setFullYear(currentYear, 0, 1);
+      return {
+         startDate: new Date(year),
          endDate: new Date()
       };
    }
