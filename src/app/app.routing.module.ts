4c4654fe2242a270fb51eea34e403d1640db3cd3
Nikolay Popov <mail@lifebits.ru>
2017-06-30 17:21:22 +0700

    fix

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginComponent } from './users/login/login.component';
+import { LoginModalComponent } from './users/login-modal/login-modal.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+import { AuthGuard } from './users/auth/auth.guard';
 
 const routes: Routes = [
    {
       path: '',
       loadChildren: './+site-pages/site-pages.module#SitePagesModule'
-   },
-   {
+   }, {
       path: 'analytics',
+      canActivate: [AuthGuard],
       loadChildren: './+analytics/analytics.module#AnalyticsModule'
-   },
-   {
+   }, {
       path: 'login',
-      component: LoginComponent,
+      component: LoginModalComponent,
       outlet: 'popup'
-   },
-   {
+   }, {
       path: '**',
       component: PageNotFoundComponent
    }
