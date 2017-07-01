bd27736b6026030137dbcc2036895045ea80cc24
Nikolay Popov <mail@lifebits.ru>
2017-07-01 12:26:36 +0700

    settings up the environment for testing

--- src/app/+analytics/surgery/surgery.component.spec.ts
+++ src/app/+analytics/surgery/surgery.component.spec.ts
@@ -1,9 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DatePipe, DecimalPipe } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MdCardModule, MdInputModule, MdIconModule } from '@angular/material';
 
 import { SurgeryComponent } from './surgery.component';
 import { DataTableComponent } from '../components/data-table/data-table.component';
+import { CellFormatPipe } from '../components/data-table/cell-format.pipe';
+
+import { SortingService } from '../../services/sorting/sorting.service';
 
 describe('SurgeryComponent', () => {
    let component: SurgeryComponent;
@@ -11,8 +17,9 @@ describe('SurgeryComponent', () => {
 
    beforeEach(async(() => {
       TestBed.configureTestingModule({
-         imports: [ ReactiveFormsModule, MdCardModule, MdInputModule, MdIconModule ],
-         declarations: [ SurgeryComponent, DataTableComponent ],
+         imports: [ HttpModule, ReactiveFormsModule, MdCardModule, MdInputModule, MdIconModule, BrowserAnimationsModule ],
+         declarations: [ SurgeryComponent, DataTableComponent, CellFormatPipe ],
+         providers: [ SortingService, DatePipe, DecimalPipe ]
       })
          .compileComponents();
    }));
--- src/app/+analytics/therapy/therapy.component.spec.ts
+++ src/app/+analytics/therapy/therapy.component.spec.ts
@@ -1,25 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DatePipe, DecimalPipe } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdCardModule, MdInputModule, MdIconModule } from '@angular/material';
 
 import { TherapyComponent } from './therapy.component';
+import { DataTableComponent } from '../components/data-table/data-table.component';
+import { CellFormatPipe } from '../components/data-table/cell-format.pipe';
+
+import { SortingService } from '../../services/sorting/sorting.service';
 
 describe('TherapyComponent', () => {
-  let component: TherapyComponent;
-  let fixture: ComponentFixture<TherapyComponent>;
+   let component: TherapyComponent;
+   let fixture: ComponentFixture<TherapyComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TherapyComponent ]
-    })
-    .compileComponents();
-  }));
+   beforeEach(async(() => {
+      TestBed.configureTestingModule({
+         imports: [ HttpModule, ReactiveFormsModule, MdCardModule, MdInputModule, MdIconModule, BrowserAnimationsModule ],
+         declarations: [ TherapyComponent, DataTableComponent, CellFormatPipe ],
+         providers: [ SortingService, DatePipe, DecimalPipe ]
+      })
+         .compileComponents();
+   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TherapyComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+   beforeEach(() => {
+      fixture = TestBed.createComponent(TherapyComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+   it('should be created', () => {
+      expect(component).toBeTruthy();
+   });
 });
